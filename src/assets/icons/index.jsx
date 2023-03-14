395b12e8a363ef4cb78a304c67db748e78d6abc1
metacader <tech@metacade.co>
2023-03-14 19:43:27 +0300

    mcd-193 - add icons and images

---
+++ src/assets/icons/index.jsx
@@ -0,0 +1,22 @@
+export { default as SearchIcon } from "./searchIcon.svg";
+export { default as PacmanIcon } from "./pacmanIcon.svg";
+export { default as GlobeIcon } from "./globeIcon.svg";
+export { default as NotificationIcon } from "./notificationIcon.svg";
+
+// Navigation Icons
+
+export { default as HomeIcon } from "./homeIcon.svg";
+export { default as TrendingIcon } from "./trendingIcon.svg";
+export { default as ArcadeIcon } from "./arcadeIcon.svg";
+export { default as CommunityIcon } from "./communityIcon.svg";
+export { default as LaunchpadIcon } from "./launchpadIcon.svg";
+export { default as GrantsIcon } from "./grantsIcon.svg";
+
+// Social Icons
+
+export { default as InstagramIcon } from "./instagramIcon.svg";
+export { default as LinkedinIcon } from "./linkedinIcon.svg";
+export { default as DiscordIcon } from "./discordIcon.svg";
+export { default as TelegramIcon } from "./telegramIcon.svg";
+export { default as TwitterIcon } from "./twitterIcon.svg";
+export { default as YoutubeIcon } from "./youtubeIcon.svg";
