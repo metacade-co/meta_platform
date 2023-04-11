895ceae00d8a7c8c78347fba36590848176805f1
metacader <tech@metacade.co>
2023-04-11 08:06:23 +0300

    mcd-193 - update games mocked database

---
+++ src/utils/games.db.js
@@ -0,0 +1,244 @@
+const games = [
+  {
+    id: 1,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/blockchainMonsterHunt.png",
+    iconURL: "",
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/blockchainMonsterHunt.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/blockchainMonsterHunt.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/blockchainMonsterHunt.png",
+    ],
+    name: "Blockchain Monster Hunt",
+    tags: ["Driving", "Shooter"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 2,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/bombCrypto.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/bombCrypto.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/bombCrypto.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/bombCrypto.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Bomb Crypto",
+    tags: ["Strategy", "Driving"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 3,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/cyberArena.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/cyberArena.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/cyberArena.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/cyberArena.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Cyber Arena",
+    tags: ["RPG", "Shooter"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 4,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/deltaTime.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/deltaTime.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/deltaTime.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/deltaTime.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Delta Time",
+    tags: ["Fighting", "Driving"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 5,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/divineJustice.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/divineJustice.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/divineJustice.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/divineJustice.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Divine Justice 2",
+    tags: ["Strategy", "Shooter"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 6,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/leagueKingdoms.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/leagueKingdoms.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/leagueKingdoms.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/leagueKingdoms.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "League Kingdoms 2",
+    tags: ["Strategy", "Driving"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 7,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/blockchainMonsterHunt.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/blockchainMonsterHunt.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/blockchainMonsterHunt.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/blockchainMonsterHunt.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Blockchain Monster Hunt 2",
+    tags: ["RPG", "Shooter"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 8,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/bombCrypto.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/bombCrypto.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/bombCrypto.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/bombCrypto.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Bomb Crypto 2",
+    tags: ["Strategy", "RPG"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 9,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/cyberArena.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/cyberArena.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/cyberArena.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/cyberArena.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Cyber Arena 2",
+    tags: ["Shooter", "RPG"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 10,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/deltaTime.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/deltaTime.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/deltaTime.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/deltaTime.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Delta Time 2",
+    tags: ["Fighting", "RPG"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 11,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/pegaxy.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/pegaxy.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/pegaxy.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/pegaxy.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Pegaxy",
+    tags: ["Strategy", "Fighting"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 12,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/publicLandHunter.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/publicLandHunter.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/publicLandHunter.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/publicLandHunter.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Public Land Hunter",
+    tags: ["Strategy", "Fighting"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 13,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/splinterLands.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/splinterLands.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/splinterLands.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/splinterLands.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Splinter Lands",
+    tags: ["Shooter", "RPG"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 14,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/undisputed.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/undisputed.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/undisputed.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/undisputed.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Undisputed",
+    tags: ["Strategy", "Fighting"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+  {
+    id: 15,
+    imageURL: "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/wolfgame.png",
+    iconURL: "",
+    gameScreenshots: [
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/wolfgame.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/wolfgame.png",
+      "https://metacade-files.fra1.digitaloceanspaces.com/images/gamesImages/256x136/wolfgame.png",
+    ],
+    description:
+      "The battles take place in arenas full of ramps, platforms and hazards - hiding is not an option as the sudden-death mechanics are only seconds away from assisting one of you into the afterlife. <br /><br /> In the courses of a match, you must do anything in your power to SMASH your opponent’s head - be it through collisions, weaponry or just “gently” nudging them in the direction of career-ending hazards. When health points run out, it’s game over. Will you be the last one standing?",
+    name: "Wolfgame",
+    tags: ["RPG", "Shooter"],
+    published: "May 4, 2023",
+    publisher: "Flowstate Web3 Arcade",
+  },
+];
+
+export default games;
